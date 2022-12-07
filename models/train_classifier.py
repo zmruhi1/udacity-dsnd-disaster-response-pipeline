312793bfd971d79aa52d49458bef687e7c81301e
Zurana Mehrin Ruhi <zurana.mehrin.ruhi1@gmail.com>
2022-12-07 20:00:48 +0600

    completed ML preparation

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,74 @@
 import sys
+import re
+import pickle
+import pandas as pd
+from sqlalchemy import create_engine
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+nltk.download(['punkt', 'wordnet', 'omw-1.4'])
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMessages', engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names = list(df.columns[4:])
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # creating tokens
+    words = word_tokenize(re.sub(r"[^a-zA-Z0-9]", ' ', text.lower()))
+    # stemming
+    stemmed_words = [PorterStemmer().stem(w) for w in words]
+    # lemmatizing
+    lemmatizer = WordNetLemmatizer()
+    lemmed_words = [lemmatizer.lemmatize(w, pos='n').strip() for w in stemmed_words]
+    lemmed_words = [lemmatizer.lemmatize(w, pos='v').strip() for w in stemmed_words]
+    
+    return lemmed_words
 
-
+    
 def build_model():
-    pass
+    pipeline = Pipeline([
+                        ('vect', CountVectorizer(tokenizer=tokenize)),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                        ])
+
+    parameters = {'clf__estimator__n_estimators': [20, 50],
+                  'vect__max_df': (0.5, 0.75, 1.0),
+                  'clf__estimator__min_samples_split': [2, 3, 5],
+                 }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    preds = model.predict(X_test)
+    
+    for i in range(len(category_names)):
+        print(Y_test.columns[i], ':')
+        print(classification_report(Y_test.iloc[:,i], preds[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
@@ -38,7 +88,7 @@ def main():
         evaluate_model(model, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        save_model(model.best_estimator_, model_filepath)
 
         print('Trained model saved!')
 
